e3c5f71d1f7a880ea4bdeddf31935160251e0d0b
Aleksandra Filipkowska <ola-filipkowska12@o2.pl>
2022-05-02 11:31:43 +0200

    Code refractoring

--- myWebsite.js
+++ myWebsite.js
@@ -1,12 +1,35 @@
-$(".seeMoreButton").on("click", changeText);
+// $(".seeMoreButton").on("click", changeText);
 
 
-function changeText() {
-  if (document.querySelector(".seeMoreButton").innerText === "Click to see more") {
-    document.querySelector(".seeMoreButton").innerText = "Click to see less";
+
+// function changeText() {
+//   if (document.querySelector(".seeMoreButton").innerText === "Click to see more") {
+//     document.querySelector(".seeMoreButton").innerText = "Click to see less";
+//   }
+//   else if (document.querySelector(".seeMoreButton").innerText === "Click to see less") {
+//     document.querySelector(".seeMoreButton").innerText = "Click to see more";
+//   }
+
+// }
+
+const toggleBtn = document.querySelector(".nav-toggle-btn");
+const navItems = document.querySelector(".nav-links");
+
+toggleBtn.addEventListener('click', function(){
+  navItems.classList.toggle('show-nav-items');
+});
+
+const seeMoreBtn = document.querySelector(".see-more-btn");
+const experienceList = document.querySelector(".experience-list");
+
+seeMoreBtn.addEventListener('click', function(){
+  experienceList.classList.toggle('show-prof-experience');
+  if (seeMoreBtn.innerText === "Click to see more") {
+    seeMoreBtn.innerText = "Click to see less";
   }
-  else if (document.querySelector(".seeMoreButton").innerText === "Click to see less") {
-    document.querySelector(".seeMoreButton").innerText = "Click to see more";
+  else {
+    seeMoreBtn.innerText = "Click to see more";
   }
-
 }
+);
+
